cdfa6555ab128e7e7fcd78fe64c61f03eeb2dc73
sdcloudtraining <79129972+sdcloudtraining@users.noreply.github.com>
2021-02-17 16:07:50 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,132 @@
+
+// STEP 1
+let someMonth;
+function theMonth();
+let currentMonth;
+let summerMonth;
+let myLibraryFunction;
+
+// STEP 2
+
+3.9 // numeric literal 
+"Hello!" // string literal 
+false // Boolean literal 
+null // literal null value 
+
+// STEP 3
+
+let anExpression = 3 * (4 / 5) + 6;
+let solarWinds {
+    name:  '',
+    warn:() => {
+        console.warn('Intruder Alert' + this.name )
+    }
+}
+
+// STEP 4
+
+ let strFirstName;
+ let strLastName; 
+ let strAddress; 
+ let strCity; 
+ let strState; 
+ let intZipCode; 
+ let intYourAge; 
+ let strReferralSource;
+ let boolMayWeContactYou;
+
+// STEP 5
+
+let strFirstName="Joe";
+let strLastName = strFirstName,
+    strAddress = "1234 Main Street"; 
+
+// STEP 6
+
+let asdf = 6 + "six";
+console.log (asdf);
+
+// STEP 7
+
+let first = true + 'asdf';
+console.log (first);
+
+let second = 1 + true;
+console.log (second);
+
+
+// STEP 8
+
+let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+window.console.log(someString);
+
+
+
+
+// STEP 9
+
+let first = null;
+let second;
+
+console.log (first + second);
+
+// STEP 10
+
+console.log (typeof "asdf");
+console.log (typeof 123);
+console.log (typeof true);
+let objAsdf = {};
+console.log (typeof objAsdf);
+console.log (typeof undefined);
+
+// STEP 11
+window.alert("Hello Kam, " +"welcome to the Javscript class!" );
+// STEP 12
+let strName="Kam";
+window.alert("Hello" + strName + ", welcome to the Javascript class!");
+
+// STEP 13
+
+let strCourse = "Javascript"
+
+window.alert ("Hello" + strName +", welcome to " + strCourse + " class!");
+
+
+// STEP 14
+let strCourse = "Javascript"
+
+window.alert ("Hello" + strName +", <br> Welcome to " + strCourse + " class!");
+// STEP 15
+
+let strName = prompt("What's your name?");
+
+let strCourse = "Javascript"
+
+window.alert ("Hello" + strName +", <br> Welcome to " + strCourse + " class!");
+// STEP 16
+let strName = prompt("What's your name?");
+let strCourse = prompt("What is your course?");
+
+window.alert ("Hello" + strName +", <br> Welcome to " + strCourse + " class!");
+// STEP 17
+
+let x = 10;
+let y = 20;
+console.log(x+y);
+// STEP 18
+let x = 20;
+x=x+20;
+console.log(x);
+// STEP 19
+let x=20;
+x = 20 * 5;
+console.log(x);
+// STEP 20
+let x = 20%3;
+x= x/1
+console.log (x);
+// STEP 21
+if (3>2){ console.log("true");}
+// STEP 22
+if (3==2) {console.log("true");}
+else{console.error("false");}
